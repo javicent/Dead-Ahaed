bfa7ac9e5851b764416cc1599dd5f102431e9ddb
javicent <103406577+javicent@users.noreply.github.com>
2022-04-25 03:21:40 -0700

    Update Rocket.js

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,41 +1,34 @@
-// Rocket prefab
-class Rocket extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, texture, frame) {
+class Player extends Phaser.GameObjects.Sprite
+{
+    constructor(scene, x, y, texture, frame)
+    {
         super(scene, x, y, texture, frame);
 
-        scene.add.existing(this);   // add to existing, displayList, updateList
-        this.isFiring = false;      // track rocket's firing status
-        this.moveSpeed = 10;         // pixels per frame
-        this.sfxRocket = scene.sound.add('sfx_rocket')  // add rocket sfx
+        // add object to existing scene
+        scene.add.existing(this);
+
+        // track the rockets firing status
+        this.isFiring = false;
+        this.moveSpeed = 10; 
+        this.sfxRocket = scene.sound.add("sfx_rocket"); // add player sfx
     }
 
-    update() {
+
+    update()
+    {
         // left/right movement
-        
         if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
             this.x -= this.moveSpeed;
         } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
             this.x += this.moveSpeed;
         }
-        
-        // // fire button
-        // if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
-        //     this.isFiring = true;
-        //     this.sfxRocket.play();
-        // }
-        // // if fired, move up
-        // if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
-        //     this.y -= this.moveSpeed;
-        // }
-        // // reset on miss
-        // if(this.y <= borderUISize * 3 + borderPadding) {
-        //     this.reset();
-        // }
     }
 
-    // reset rocket to "ground"
-    reset() {
+    // reset player 
+    reset()
+    {
         this.isFiring = false;
-        this.y = game.config.height - borderUISize - borderPadding;
+        this.y = 400;
     }
 }
+
